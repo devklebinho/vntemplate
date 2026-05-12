9fdde11a17a1772dadc4aee5cb56952c057a1262
devklebinho <contato@klebersilva.dev.br>
2026-05-12 14:54:19 -0300

    Creating a Dialogue Manager

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,17 @@
+using TMPro;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] TMP_Text ChararacterName;
+    [SerializeField] TMP_Text Dialog;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
