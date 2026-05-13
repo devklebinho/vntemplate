3e4c4978b8899973acd006d68bce91fd520f1352
devklebinho <contato@klebersilva.dev.br>
2026-05-13 17:44:27 -0300

    Dialogue Manager and Scriptable Dialogue

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue/Dialogue Data")]
+public class DialogueData : ScriptableObject
+{
+    public List<DialogueLine> Lines = new();
+}
+
+[System.Serializable]
+public class DialogueLine
+{
+    public string CharacterName;
+
+    [TextArea(3, 6)]
+    public string DialogueText;
+}
--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -3,15 +3,54 @@ using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
 {
-    [SerializeField] TMP_Text ChararacterName;
-    [SerializeField] TMP_Text Dialog;
-    void Start()
+    [Header("UI")]
+    [SerializeField] private GameObject dialogueBox;
+    [SerializeField] private TMP_Text characterName;
+    [SerializeField] private TMP_Text dialogue;
+
+    [Header("Dialogue")]
+    [SerializeField] private DialogueData currentDialogue;
+
+    [SerializeField] private int currentLineIndex;
+
+    private void Start()
     {
-        
+        StartDialogue(currentDialogue);
     }
 
-    void Update()
+    public void StartDialogue(DialogueData dialogueData)
     {
-        
+        currentDialogue = dialogueData;
+        currentLineIndex = 0;
+
+        dialogueBox.SetActive(true);
+
+        ShowCurrentLine();
+    }
+
+    public void NextDialogue()
+    {
+        currentLineIndex++;
+
+        if (currentLineIndex >= currentDialogue.Lines.Count)
+        {
+            EndDialogue();
+            return;
+        }
+
+        ShowCurrentLine();
+    }
+
+    private void ShowCurrentLine()
+    {
+        DialogueLine line = currentDialogue.Lines[currentLineIndex];
+
+        characterName.text = line.CharacterName;
+        dialogue.text = line.DialogueText;
+    }
+
+    private void EndDialogue()
+    {
+        dialogueBox.SetActive(false);
     }
 }
